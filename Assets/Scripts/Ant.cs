f3b49e8b17cf89f92188427a9abe4b38f3af3224
gdgfgf123-creator <gdgfgf123@gmail.com>
2025-10-21 16:10:26 +0700

    Ant welk from point A to B

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -17,15 +17,33 @@ public class Ant : Enemy
     public override void Behavior()
     {
         rd.MovePosition(rd.position + velocity * Time.fixedDeltaTime);
-
+        //move left และเกินขอบซ้าย
+        if (velocity.x < 0 && rd.position.x <= movePoints[0].position.x)
+        {
+            Flip();
+        }
+        //move right และเกินขอบขวา
+        if (velocity.x > 0 && rd.position.x >= movePoints[1].position.x)
+        {
+            Flip();
+        }
+    }
+    public void Flip()
+    {
+        velocity.x *= -1; //change direction of movement
+                          //Flip the image
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
     private void FixedUpdate()
     {
         Behavior();
+        
     }
     // Update is called once per frame
     void Update()
     {
-           
+            
     }
 }
