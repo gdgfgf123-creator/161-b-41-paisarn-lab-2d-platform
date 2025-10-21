a6717f2b3b4e5b5ec273d7c4191fdf1a33dc6215
gdgfgf123-creator <gdgfgf123@gmail.com>
2025-10-21 16:38:47 +0700

    Croccodile Range player ,shoot

--- Assets/Scripts/Croccodile.cs
+++ Assets/Scripts/Croccodile.cs
@@ -2,20 +2,35 @@ using UnityEngine;
 
 public class Croccodile : Enemy
 {
-    public override void Behavior()
-    {
-        throw new System.NotImplementedException();
-    }
+    [SerializeField] float atkRange;
+    public player player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Intialize(50);
+        DamgeHit = 30;
+        //set atk range and target
+        atkRange = 6.0f;
+        player = GameObject.FindFirstObjectByType<player>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Behavior();
+    }
+    public override void Behavior()
+    {
+        //find distance between Croccodile and Player
+        Vector2 distance = transform.position - player.transform.position;
+        if (distance.magnitude <= atkRange)
+        {
+            Debug.Log($"{player.name} is in the {this.name}'s atk range!");
+            Shoot();
+        }
+    }
+    public void Shoot()
+    {
+        Debug.Log($"{this.name} shoots rock to the {player.name}!");
+
     }
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -8,6 +8,18 @@ public class player : Character
         base.Intialize(100);
     }
 
+    public void OnHitWith(Enemy enemy)
+    {
+        TakeDamage(enemy.DamgeHit);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Enemy enemy = other.gameObject.GetComponent<Enemy>();
+        if (enemy != null){
+            OnHitWith(enemy);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
